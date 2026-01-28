bc8494d54793773953166885d47a8a718b3e79c0
shymaasaed <shymaasaeed404@gmail.com>
2026-01-28 18:06:58 +0200

    dev

--- stress.js
+++ stress.js
@@ -6,7 +6,7 @@ export const options = {
   duration: '10s',
 
   thresholds: {
-    http_req_failed: ['rate<0.01'],   
+    http_req_failed: ['rate<0.01'],  
     http_req_duration: ['p(95)<1000'], 
     checks: ['rate>0.99'],           
   },
